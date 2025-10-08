2b59d717dc2a5414cc76d296554b5ff5e98d6776
Ana Beatriz <ana.cezar3@etec.sp.gov.br>
2025-10-07 22:32:46 -0300

    no se

--- telas/TelaDescobrir.jsx
+++ telas/TelaDescobrir.jsx
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-function TelaDescobrir() {
+function TelaDescobrir ({ navigation }) {
     return (
         <View>
             <Text>Tela Descobrir</Text>
+
+            <Button
+                title="Ir para Login"
+                onPress={() => navigation.navigate("Login")}
+            />
         </View>
     );
 }
