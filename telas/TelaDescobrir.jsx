be4a030b1b23d2e9f730fd0d2d56918bc0a2ca48
anabia278 <ana.teixeira127@etec.sp.gov.br>
2025-11-06 19:39:31 -0300

    tela descobrir

--- telas/TelaDescobrir.jsx
+++ telas/TelaDescobrir.jsx
@@ -1,6 +1,31 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { View, Text } from 'react-native';
 
+import axios from 'axios';
+
 function TelaDescobrir() {
+    const [usuarios, setUsuarios] = useState([]);
+
+    // Vai executar quando o componente for montado
+    useEffect(() => {
+        buscaUsuarios();
+    }, []);
+
+    // Busca os usuários cadastrados
+    const buscaUsuarios = async () => {
+        try {
+            // Conectar no servidor para buscar os usuários
+            const resposta = await axios.get("http://10.0.8.58:3000/api/usuarios");
+
+            // Atualiza o estado com os usuários recebidos
+            setUsuarios(resposta.data);
+
+            alert('Carreguei os usuários com sucesso!');
+        } catch (error) {
+            console.error('Erro ao buscar usuários:', error);
+        }
+    }
+
     return (
         <View>
             <Text>Tela de Descobrir</Text>
